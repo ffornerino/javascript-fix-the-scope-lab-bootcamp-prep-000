f7a097016fcdc00fe96babaa0e6107a95ba9b325
Fabiana Fornerino <fabianafornerino@gmail.com>
2019-08-13 22:25:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var animal = 'dog'
-
 function myAnimal() {
   return animal
 }
 
+animal = 'cat'
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
@@ -14,7 +14,6 @@ function yourAnimal() {
 function add2(n) {
   const two = 2
   return parseInt(n, 10) + two
-
 }
 
 var funkyFunction = function() {
@@ -25,5 +24,5 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-function()
+funkyFunction();
 var theFunk = funkyFunction();
